e4011addc059cf14c6c5dc464f0dbb64546ca6ca
FrancoL42 <francolentini06@gmail.com>
2023-10-30 14:56:59 -0300

    clase helper y patron dao y factory

---
+++ Cines/Cines/Acceso a Datos/Dao/IDao.cs
@@ -0,0 +1,17 @@
+﻿using Cines.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaCineBack.Acceso_a_Datos.Dao
+{
+    interface IDao
+    {
+        bool crear(Funciones funcion);
+        bool borrar(Funciones funcion);
+        int proximoID();
+        List<Asientos> consultar();
+    }
+}
